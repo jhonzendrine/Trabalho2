f19d39b0347ac80a0619321201796b707ee92241
Jhonatan <jhonatan.zendrine@gmail.com>
2022-09-09 19:40:52 -0300

    Versão 2.1

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ namespace INF099
     {
         static void Main(string[] args) {
             //Versão 1
-            //Versão 2
+            //Versão 2 
             //Versão 3
+
             Console.WriteLine("Olá Professor!");
             //Versão 3.2 Conflito
+            //Versão 2.1
         }
         //Versão 3.1
     }
