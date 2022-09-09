c56de9a06c0c0e8023a91c4c75cf8141754f084e
Jhonatan <jhonatan.zendrine@gmail.com>
2022-09-09 19:38:46 -0300

    Versão 1

--- Program.cs
+++ Program.cs
@@ -3,9 +3,8 @@ namespace INF099
     public class OlaMundo
     {
         static void Main(string[] args) {
-            //Alteração realizada por JHON
-            //Outra Alteração realizada por Jhon
-		    Console.WriteLine("Olá Professor!");
+            //Versão 1
+            Console.WriteLine("Olá Professor!");
         }
     }
 }
