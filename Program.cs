a20cf773be52aefcbd7aeef8aada1b5a0bbe3351
jhonzendrine <57139072+jhonzendrine@users.noreply.github.com>
2022-09-09 19:32:19 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace INF099
+{
+    public class OlaMundo
+    {
+        static void Main(string[] args) {
+            //Alteração realizada por JHON
+            //Outra Alteração realizada por Jhon
+		    Console.WriteLine("Olá Professor!");
+        }
+    }
+}
